6dfcda3e6f97f49f76438d4aea38b60f0d5531b1
Preetraj Haldar <preetrajhaldar2002@gmail.com>
2025-11-25 12:03:11 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,10 @@ const config: CapacitorConfig = {
   appName: 'Khelconnect',
   webDir: 'out',
   // Add this for live development ONLY (Remove before publishing!)
-  server: {
+/*   server: {
     url: 'http://192.168.0.139:3000', // Replace with your computer's IP
     cleartext: true
-  }
+  } */
 };
 
 export default config;
